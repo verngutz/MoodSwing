8a989304b542fd49e519864d2a7e1d93fde51c90
unknown <Jans@.(none)>
2010-12-05 12:41:30 +0800

    Added game screens feature

--- MoodSwingGUI/MSButton.cs
+++ MoodSwingGUI/MSButton.cs
@@ -15,18 +15,51 @@ namespace MoodSwingGUI
 {
     public class MSButton:DrawableGameComponent
     {
+        enum ButtonState {
+            UNCLICKED = 0,
+            CLICKED,
+            HOVERED
+        }
+
         private Rectangle boundingRectangle;
-        private Texture2D currentTexture;
+        private ButtonState currentState;
         private Texture2D clickedTexture;
         private Texture2D hoveredTexture;
         private Texture2D unclickedTexture;
         private Color highlight;
         private MSLabel label;
+        private SpriteBatch spriteBatch;
 
-        public MSButton(Game g)
+        public MSButton(Game g, int x, int y, int width, int height,
+            Texture2D unclicked, Texture2D clicked, Texture2D hovered, SpriteBatch sb, Color hlight )
             : base(g)
         {
+            unclickedTexture = unclicked;
+            clickedTexture = clicked;
+            hoveredTexture = hovered;
+            spriteBatch = sb;
+            highlight = hlight;
+            boundingRectangle = new Rectangle( x, y, width, height );
+        }
 
+        public override void Draw(GameTime gameTime)
+        {
+            Texture2D currTexture;
+            switch (currentState)
+            {
+                case ButtonState.CLICKED:
+                    currTexture = clickedTexture;
+                    break;
+                case ButtonState.HOVERED:
+                    currTexture = hoveredTexture;
+                    break;
+                case ButtonState.UNCLICKED:
+                    currTexture = unclickedTexture;
+                    break;
+            }
+
+            spriteBatch.Draw(currTexture, boundingRectangle, highlight);
+            base.Draw(gameTime);
         }
     }
 }
---
+++ MoodSwingGame/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MoodSwingGame
+{
+    public class MainMenu : MoodSwingScreen
+    {
+        private static MainMenu mainMenu = null;
+        public static MainMenu MAIN_MENU
+        {
+            get {
+                return mainMenu; 
+            }
+        }
+
+        private MainMenu(Texture2D texture, Game1 g, SpriteBatch sb)
+            : base(texture, g, sb)
+        {
+
+        }
+
+        public static void INIT(Texture2D t2D, Game1 game, SpriteBatch sb)
+        {
+            mainMenu = new MainMenu(t2D, game, sb);
+        }
+    }
+}
