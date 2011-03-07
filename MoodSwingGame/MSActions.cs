1add080fe005c6e78086d2b88bebda58092beb8d
Vernon G <vern_go_123@yahoo.com>
2011-03-08 02:09:59 +0800

    Added new Textures, new map, and (unfinished) Gameplay Instructions and Game Summary.

--- MoodSwingGame/MSActions.cs
+++ MoodSwingGame/MSActions.cs
@@ -72,7 +72,7 @@ namespace MoodSwingGame
         {
             MoodSwing moodSwing = (MoodSwing)game;
             moodSwing.CurrentScreen.ResetHovers();
-            moodSwing.CurrentScreen = new MSDistrictScreen(@"Content\mapinfo.txt", moodSwing);
+            moodSwing.CurrentScreen = new MSDistrictScreen(@"Content\Pandalevel2.txt", moodSwing);
             MSNotifier.GetInstance().ClearNotifications();
             MSNotifier.GetInstance().FreezeNotifications = false;
         }
