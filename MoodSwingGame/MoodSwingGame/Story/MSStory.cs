ecd342f2808fe462486b602401653e1d3a606d53
Vernon G <vern_go_123@yahoo.com>
2011-04-21 00:12:52 +0800

    Added support for story.

---
+++ MoodSwingGame/MoodSwingGame/Story/MSStory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+using MoodSwingCoreComponents;
+using MoodSwingGUI;
+
+namespace MoodSwingGame
+{
+    public static class MSStory
+    {
+        //public static bool eventVariable1; etc. etc.
+
+        private static List<MSStoryEvent> timeActiveEvents = new List<MSStoryEvent>();
+
+        public static void AddStoryEvent(MSStoryEvent e)
+        {
+            timeActiveEvents.Add(e);
+        }
+
+        public static void RemoveStoryEvent(MSStoryEvent e)
+        {
+            timeActiveEvents.Remove(e);
+        }
+
+        private static void Update(int time, Game game)
+        {
+            foreach(MSStoryEvent e in timeActiveEvents)
+            {
+                if (e.Enabled())
+                {
+                    e.PerformAction(game);
+                }
+            }
+
+            /**
+             * if (time == 0)
+             * {
+             *      AddStoryEvent(blah); etc. etc.
+             * }
+             */
+        }
+    }
+}
---
+++ MoodSwingGame/MoodSwingGame/Story/MSStoryEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+using MoodSwingCoreComponents;
+using MoodSwingGUI;
+
+namespace MoodSwingGame
+{
+    public abstract class MSStoryEvent : MSAction
+    {
+        public abstract bool Enabled();
+        public abstract void PerformAction(Game game);
+    }
+}
