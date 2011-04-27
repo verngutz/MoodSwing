96cf90ad13df0e83b7abfdcdc36ea1aa785669c2
Janssen <jlgo172003@yahoo.com>
2011-04-28 03:51:27 +0800

    FIXED ERRORS.

--- MoodSwingGame/MoodSwingGame/ParametricCurves/GenericMobCurve.cs
+++ MoodSwingGame/MoodSwingGame/ParametricCurves/GenericMobCurve.cs
@@ -12,7 +12,7 @@ namespace MoodSwingGame
     {
         public int X(int t) { return X(t, 0); }
 
-        public int X(int t, int x0) { return (int)(MathHelper.Clamp((float)(0.0025f*(Math.Log(t)*Math.Cos(t*t) + x0) * 100f), 0f, 100f)); }
+        public int X(int t, int x0) { return (int)(MathHelper.Clamp((float)(0.005f*(Math.Log(t)*Math.Cos(t*t) + x0) * 60), 0f, 100f)); }
 
         public int Dx(int t) { return -1; }
     }
